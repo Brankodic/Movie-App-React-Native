95cfeaaa850478edde69f587253744727375b4af
Brankodic <srdjan.brankovicc@gmail.com>
2020-09-10 22:23:37 +0200

    Basic nav setup

--- App.js
+++ App.js
@@ -1,23 +1,23 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import {Home, MovieDetailsScreen} from './src/screens';
+
+const Stack = createStackNavigator();
 
 class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Movie App</Text>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="MovieDetails" component={MovieDetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
